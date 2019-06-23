c0ccc3adb1059d64b75a66d71242f8aab285489c
Atrebas <Atrebas@users.noreply.github.com>
2019-06-23 16:10:21 +0200

    show-hide console outputs for python code - minor updates

--- js/show-hide-console-outputs.js
+++ js/show-hide-console-outputs.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('#hideshow').on('click', function(event) {        
          console.log("click");
-         $('pre').not('.r').toggle('show');
+         $('pre').not('.r, python').toggle('show');
          if (this.value=="Show the outputs") this.value = "Hide the outputs";
          else this.value = "Show the outputs";
     });
